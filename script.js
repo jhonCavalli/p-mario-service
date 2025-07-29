a93324f886fe87fe17b1e208f0e9f8b08bae0d87
Jhon Cavalli <jhoncavalli@hotmail.com>
2025-07-28 23:26:52 -0300

    criando comentario  variaveis e funções.....

---
+++ script.js
@@ -0,0 +1,17 @@
+/* 
+
+
+variaveis - uum espaçp de memorio que vc reserva e pode 
+guardar o que quiser la dentro.
+
+3 jeito de criar variaveis
+ - var /jeito antigo, não é mais usado.
+ - let / jeito novo, mais usado - vc pode alterar o valor dela.
+ - const / consntante - vc não pode alterar o valor dela, ela é fixa. 
+
+
+ funçoes - é um bloco de codigo que vc pode chamar quantas vezes quiser 
+ um trecho de codigo que , so e executado,quando vc chamado.
+
+
+ */
